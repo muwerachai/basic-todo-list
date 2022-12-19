3eeeebc3cba08759a714c093d9d82279fddb44a2
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-12-19 08:19:16 +0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { v4 as uuidv4} from 'uuid';
+
 import EditBar from './component/EditBar';
 import Pagination from './component/Pagination';
 import SearchBox from './component/SearchBox';
@@ -5,7 +8,15 @@ import SearchStatus from './component/SearchStatus';
 import ShowAmount from './component/ShowAmount';
 import ShowItem from './component/ShowItem';
 import TodoList from './component/TodoList';
+
+const initialTodos = [
+  {id: uuidv4(), title: 'shopping',completed: true},
+  {id: uuidv4(), title: 'dinner',completed: false},
+  {id: uuidv4(), title: 'swimming',completed: false}
+]
+
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
   return (
     <div className="container mt-5 mb-3 max-w-xs">
       <EditBar />
@@ -14,7 +25,7 @@ function App() {
       <SearchStatus />
       </div>
       <ShowItem />
-      <TodoList />
+      <TodoList todos={todos}/>
       <div className="d-flex justify-content-between align-items-center">
       <ShowAmount />
       <Pagination />
