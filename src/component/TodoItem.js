429d947d27dbf3177c0c615b9fe9111d519fd593
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-12-22 02:21:16 +0700

    add pagination

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import TodoContent from './TodoContent';
+import EditBar from './EditBar';
 
 function TodoItem(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEditing = () => {
+    setIsEditing(prevIsEditing => !prevIsEditing);
+  };
+
   return (
     <li
       className={`list-group-item p-3 border-l-2 border-l-${
         props.completed ? 'success' : 'warning'
       }`}
     >
-      <TodoContent title={props.title} completed={props.completed} />
+      {isEditing ? (
+        <EditBar
+        toggleEditing={toggleEditing}
+        updateTodo={props.updateTodo}
+        id = {props.id}
+        title={props.title} 
+        />
+      ) : (
+      <TodoContent
+      id = {props.id}
+      title={props.title} 
+      completed={props.completed} 
+      removeTodo={props.removeTodo}
+      toggleTodo={props.toggleTodo}
+      toggleEditing={toggleEditing}
+      />
+      )}
     </li>
   );
 }
